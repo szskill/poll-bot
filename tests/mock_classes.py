82c61b412e3c6e84814bcce2bb9d482e47db9498
szskill <lumonexu@gmail.com>
2022-06-24 15:03:11 +0800

    feat(tests): Docstrings for mock classes

--- tests/mock_classes.py
+++ tests/mock_classes.py
@@ -1,10 +1,14 @@
 class MockMember:
+    """Mock class for :code:`discord.Member`."""
+
     def __init__(self, display_name: str | None, display_avatar: str | None) -> None:
         self.display_name = display_name
         self.display_avatar = display_avatar
 
 
 class MockCommand:
+    """Mock class for :code:`discord.ext.commands.Command`."""
+
     def __init__(self, name: str | None, help: str | None) -> None:
         self.name = name
         self.help = help
